a330e89d4bbfff2c4ac31cd62c03487b2f7a78ad
chainsawsteel1 <shimasoushun@gmail.com>
2023-09-27 22:34:17 +0900

    ノート内容指定

--- src/modules/note/index.ts
+++ src/modules/note/index.ts
@@ -1,6 +1,8 @@
 import autobind from 'autobind-decorator';
 import Module from '@/module';
 import Message from '@/message';
+import { genItem } from '@/vocabulary';
+import serifs from '@/serifs';
 
 export default class extends Module {
 	public readonly name = 'note';
@@ -16,9 +18,21 @@ export default class extends Module {
 	private async mentionHook(msg: Message) {
 		if (msg.text && msg.text.includes('ノートして')) {
 			setTimeout(() => {
-				this.ai.api('notes/create', {
-					renoteId: note.id
-				});
+				const notes = [
+					...serifs.noting.notes,
+					() => {
+						const item = genItem();
+						return serifs.noting.want(item);
+					},
+					() => {
+						const item = genItem();
+						return serifs.noting.see(item);
+					},
+					() => {
+						const item = genItem();
+						return serifs.noting.expire(item);
+					},
+				];
 			}, 3000);
 			return true;
 		} else {
