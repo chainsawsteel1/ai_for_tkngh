ffac84bdaccb1484199769d294697c84f82483c0
chainsawsteel1 <shimasoushun@gmail.com>
2023-09-27 22:42:43 +0900

    強引に

--- src/modules/note/index.ts
+++ src/modules/note/index.ts
@@ -16,27 +16,38 @@ export default class extends Module {
 
 	@autobind
 	private async mentionHook(msg: Message) {
-		if (msg.text && msg.text.includes('ノートして')) {
-			setTimeout(() => {
-				const notes = [
-					...serifs.noting.notes,
-					() => {
-						const item = genItem();
-						return serifs.noting.want(item);
-					},
-					() => {
-						const item = genItem();
-						return serifs.noting.see(item);
-					},
-					() => {
-						const item = genItem();
-						return serifs.noting.expire(item);
-					},
-				];
-			}, 3000);
+		if (msg.text && msg.text.includes('ノート')) {
+			this.post();
 			return true;
 		} else {
 			return false;
 		}
+	}
+
+	@autobind
+	private post() {
+		const notes = [
+			...serifs.noting.notes,
+			() => {
+				const item = genItem();
+				return serifs.noting.want(item);
+			},
+			() => {
+				const item = genItem();
+				return serifs.noting.see(item);
+			},
+			() => {
+				const item = genItem();
+				return serifs.noting.expire(item);
+			},
+		];
+
+		const note = notes[Math.floor(Math.random() * notes.length)];
+
+		// TODO: 季節に応じたセリフ
+
+		this.ai.post({
+			text: typeof note === 'function' ? note() : note
+		});
 	}
 }
